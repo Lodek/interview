6ba5197bcfebb6a00241c736469ae37251572663
Lodek <23224552+Lodek@users.noreply.github.com>
2020-07-22 15:39:27 -0300

    apps fix

--- app/settings/base.py
+++ app/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
     #self
     'base.apps.BaseConfig',
+
 ]
 
 MIDDLEWARE = [
--- base/apps.py
+++ base/apps.py
@@ -5,6 +5,9 @@ class BaseConfig(AppConfig):
     name = 'base'
 
     def ready(self):
+        self.create_bands()
+
+    def create_bands(self):
         from .models import Band
         bands = ['B1', 'B2', 'B3']
         for band in bands:
