a9b64f5a74d86e8dccf2bd22204956c2eefa4b4b
Lodek <23224552+Lodek@users.noreply.github.com>
2020-07-22 14:42:00 -0300

    models for base

--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 from django.contrib.auth import get_user_model
+
+class Candidate(models.Model):
+
+    name = models.CharField(255)
+
+
+class Position(models.Model):
+    pass
+
+
+class Band(models.Model):
+
+    band = models.CharField(2)
+
+    
