031dab39879b79507540fdba65de1cf0405e6ef7
Lodek <23224552+Lodek@users.noreply.github.com>
2020-07-22 15:39:05 -0300

    formatting

--- base/models.py
+++ base/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 from django.contrib.auth import get_user_model
 
-class Candidate(models.Model):
 
+class Candidate(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
 
