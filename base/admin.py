2d26d7c4b056ff43d92dd3e8475c6f4b3a135c2c
Lodek <23224552+Lodek@users.noreply.github.com>
2020-07-22 15:10:14 -0300

    admin plus fixed models

--- base/admin.py
+++ base/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+
+from .models import Position, Candidate, Band, Subarea, Area
+
+admin.site.register(Position)
+admin.site.register(Candidate)
+admin.site.register(Band)
+admin.site.register(Subarea)
+admin.site.register(Area)
--- base/models.py
+++ base/models.py
@@ -4,32 +4,33 @@ from django.contrib.auth import get_user_model
 
 class Candidate(models.Model):
 
-    name = models.CharField(255)
-    email = models.CharField(255)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
 
 
 class Position(models.Model):
-    CL = 'Closed'
-    OP = 'Open'
+    OPEN = 'OP'
+    CLOSED = 'CL'
+
     STATUS_CHOICES = [
-        (CL, 'Closed'),
-        (OP, 'Open'),
+        (CLOSED, 'Closed'),
+        (OPEN, 'Open'),
     ]
 
-    name = models.CharField(255)
-    description = models.CharField(1024)
-    status = models.CharField(max_length=2, choices=STATUS_CHOICES,
-                              default=OP)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1024)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=OPEN)
     band = models.ForeignKey('band', on_delete=models.PROTECT)
 
 
 class Band(models.Model):
-    band = models.CharField(2)
+    band = models.CharField(max_length=2)
 
     
 class Subarea(models.Model):
-    subarea = modeels.CharField(255)
+    subarea = models.CharField(max_length=255)
+    area = models.ForeignKey('area', on_delete=models.SET_NULL, null=True)
 
     
 class Area(models.Model):
-    area = modeels.CharField(255)
+    area = models.CharField(max_length=255)
