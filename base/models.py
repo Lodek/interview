19c3b6709433d2f29cd25b73bd9a4ca689136ee2
Lodek <23224552+Lodek@users.noreply.github.com>
2020-07-22 15:05:06 -0300

    base models

--- base/models.py
+++ base/models.py
@@ -5,14 +5,31 @@ from django.contrib.auth import get_user_model
 class Candidate(models.Model):
 
     name = models.CharField(255)
+    email = models.CharField(255)
 
 
 class Position(models.Model):
-    pass
+    CL = 'Closed'
+    OP = 'Open'
+    STATUS_CHOICES = [
+        (CL, 'Closed'),
+        (OP, 'Open'),
+    ]
+
+    name = models.CharField(255)
+    description = models.CharField(1024)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES,
+                              default=OP)
+    band = models.ForeignKey('band', on_delete=models.PROTECT)
 
 
 class Band(models.Model):
-
     band = models.CharField(2)
 
     
+class Subarea(models.Model):
+    subarea = modeels.CharField(255)
+
+    
+class Area(models.Model):
+    area = modeels.CharField(255)
