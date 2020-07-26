3158c702a450af712c7ba69acc7e15d4eb99af5f
Lodek <23224552+Lodek@users.noreply.github.com>
2020-07-26 19:15:13 -0300

    added candiate position and template to evaluation form

--- interview/forms.py
+++ interview/forms.py
@@ -8,15 +8,17 @@ class SetupForm(forms.Form):
     position = forms.ModelChoiceField(Position.objects.all())
     template = forms.ModelChoiceField(Template.objects.all())
 
-class ScoreForm(forms.Form):
-    SCORE_CHOICES = [(i, i) for i in range (6)]
-    score = forms.TypedChoiceField(choices=SCORE_CHOICES , 
-                                   coerce=int, empty_value=None)
 
-def score_form_factory(field_names):
+def score_form_factory(field_names, candidate_id, position_id, template_id):
     SCORE_CHOICES = [(i, i) for i in range (6)]
     fields = {name: forms.TypedChoiceField(choices=SCORE_CHOICES, coerce=int,
-                                           empty_value=None)
+                                           empty_value=None, initial=0)
               for name in field_names}
+    fields['candidate'] = forms.IntegerField(initial=candidate_id, disabled=True,
+                                             widget=forms.HiddenInput)
+    fields['template'] = forms.IntegerField(initial=template_id, disabled=True,
+                                             widget=forms.HiddenInput)
+    fields['position'] = forms.IntegerField(initial=position_id, disabled=True,
+                                             widget=forms.HiddenInput)
     fields['SCORE_CHOCIES'] = SCORE_CHOICES
     return type('ScoreForm', (forms.Form,), fields)
