f7a686637161b9464ac6874bcd212f496f4d27c6
Lodek <23224552+Lodek@users.noreply.github.com>
2020-07-28 18:19:49 -0300

    tests for calculator

--- viz/tests.py
+++ viz/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+from inteview.models import Question
+
+from .stats import StatCalculator
+
+
+class StatCalculatorTest(TestCase):
+
+    def setUp(self):
+        self.calc = StatCalculator([])
+        self.scores = [
+            (Question(weight=3), 3),
+            (Question(weight=2), 1),
+        ]
+
+    def test_calculate_avg(self):
+        avg = self.calc._calculate_avg(self.scores)
+        self.assertEqual(avg, 11/5)
+        
+   def test_calculate_avgs(self):
+       groups = {
+           'a': self.scores,
+           'b': self.scores,
+       }
+       avgs = self.calc._calculate_avgs(groups)
+       self.assertEqual(avg['a'], 11/5)
+       self.assertEqual(avg['b'], 11/5)
