a7f4736ff66af2d484c43bfb2139d71767f5a496
Lodek <23224552+Lodek@users.noreply.github.com>
2020-07-28 08:53:14 -0300

    added conlcusion url and names

--- interview/urls.py
+++ interview/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 
-from .views import setup, evaluation, review
+from .views import setup, evaluation, review, conclusion
 
 app_name = 'interview'
 urlpatterns = [
-    path('', setup),
-    path('evaluation', evaluation),
-    path('review', review),
+    path('', setup, name='setup'),
+    path('evaluation', evaluation, name='evaluation'),
+    path('review', review, name='review'),
+    path('conclusion', conclusion, name='conclusion'),
 ]
