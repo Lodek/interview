332157a7d1348779e73cb28ae1fc26a5d89a6f60
Lodek <23224552+Lodek@users.noreply.github.com>
2020-07-28 08:53:42 -0300

    conclusion view

--- interview/views.py
+++ interview/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from collections import defaultdict, namedtuple
 
 from .forms import SetupForm, score_form_factory
-from .models import Template
+from .models import Template, Question, Score, Interview
 from base.models import Candidate, Position
 
 def setup(request):
@@ -12,9 +12,9 @@ def setup(request):
 
 
 def evaluation(request):
-    request.sessions['template_id'] = request.GET['template']
-    request.sessions['candidate_id'] = request.GET['candidate']
-    request.sessions['position_id'] = request.GET['position']
+    request.session['template_id'] = request.GET['template']
+    request.session['candidate_id'] = request.GET['candidate']
+    request.session['position_id'] = request.GET['position']
 
     template_id = request.GET['template']
     template = Template.objects.get(pk=template_id)
@@ -37,4 +37,42 @@ def evaluation(request):
     return render(request, 'interview/evaluation.html', {'areas': areas})
 
 def review(request):
-    return render(request, 'interview/review.html')
+    template_id = request.session['template_id'] 
+    candidate_id = request.session['candidate_id']
+    position_id = request.session['position_id']
+
+    position = Position.objects.get(pk=position_id)
+    candidate = Candidate.objects.get(pk=candidate_id)
+    template = Template.objects.get(pk=template_id)
+
+    extract_pk = lambda key: int(key.replace('score_', ''))
+    questions_scores = [(Question.objects.get(pk=extract_pk(key)), int(score))
+                        for key, score in request.POST.items() if 'score_' in key]
+
+    return render(request, 'interview/review.html', {
+        'questions_scores': questions_scores,
+        'position': position,
+        'candidate': candidate,
+        'template': template,
+    })
+
+def conclusion(request):
+    template_id = request.session['template_id'] 
+    candidate_id = request.session['candidate_id']
+    position_id = request.session['position_id']
+
+    extract_pk = lambda key: int(key.replace('score_', ''))
+    questions_scores = {Question.objects.get(extract_pk(key)): int(score)
+                        for key, score in request.POST.items() if 'score_' in key}
+
+    interview = Interview()
+    interview.position = Position.objects.get(pk=position_id)
+    interview.candidate = Candidate.objects.get(pk=candidate_id)
+    interview.interviewer = request.user
+    interview.save()
+
+    for question, score in questions_scores.items():
+        score = Score(question=question, score=score, interview=interview)
+        score.save()
+
+    return render(request, 'interview/conclusion.html', dict(inteview=interview))
