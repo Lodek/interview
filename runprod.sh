9ff7833a3da139335b292d3a9ea8d79a302b4e62
Lodek <23224552+Lodek@users.noreply.github.com>
2020-07-29 12:36:47 -0300

    runprod

--- runprod.sh
+++ runprod.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+docker-compose --file Devops/docker-compose-prod.yml --project-directory . down
 docker-compose --file Devops/docker-compose-prod.yml --project-directory . up --build -d
 docker-compose --file Devops/docker-compose-prod.yml --project-directory . exec web python manage.py migrate
