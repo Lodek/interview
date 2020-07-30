4d2c0beb353de6b8345dea42d1f6c37624c7035f
Lodek <23224552+Lodek@users.noreply.github.com>
2020-07-30 12:53:21 -0300

    compare urls

--- viz/urls.py
+++ viz/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 
-from .views import detail, list
+from .views import detail, list, compare
 
 app_name = 'viz'
 urlpatterns = [
     path('', list, name='list'),
     path('<int:interview_id>', detail, name='detail'),
+    path('compare', compare, name='compare'),
 ]
