cfdc17ba02b52c805d6fb5634916d6edee5184c4
Lodek <23224552+Lodek@users.noreply.github.com>
2020-08-02 17:30:41 -0300

    added vertical filter to template

--- interview/admin.py
+++ interview/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import Question, Template, Interview, Score
 
 admin.site.register(Question)
-admin.site.register(Template)
 admin.site.register(Interview)
 admin.site.register(Score)
+
+
+@admin.register(Template)
+class TemplateAdmin(admin.ModelAdmin):
+    filter_vertical = ['questions']
+
+
